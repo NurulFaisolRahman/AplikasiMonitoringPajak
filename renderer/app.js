0ecd8750b8694a423a21a5eada1eaeba2f33408a
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2019-12-25 18:13:16 +0700

    FIX Data Text, Jam Buka, Kirim Data Text, Jam Tutup

--- renderer/app.js
+++ renderer/app.js
@@ -5,6 +5,7 @@ const schedule = require('node-schedule')
 const Store = require('./store.js')
 
 var SinyalOnline
+var TextUpload
 
 const store = new Store({
   configName: 'Sesi',
@@ -21,13 +22,17 @@ window.addEventListener('offline', () => {
   console.log('stop')
 })
 
+function KirimSinyalOnline() {
+  SinyalOnline = schedule.scheduleJob('*/1 * * * *', function(){
+    if (parseInt(moment().format('HH')) >= store.get('JamBuka') && parseInt(moment().format('HH')) < store.get('JamTutup')) {
+      var Data = { NPWPD: store.get('NPWPD'), Sinyal: moment().format('YYYY-MM-DD HH:mm:ss') }
+      $.post(URL+"UpdateSinyal", Data)
+      console.log('sukses')
+    }
+  });
+}
 window.addEventListener('online', () => {
   document.getElementById("Alert").style.display = 'none'
-  SinyalOnline = schedule.scheduleJob('*/5 * * * * *', function(){
-    var Data = { NPWPD: store.get('NPWPD'), Sinyal: moment().format('YYYY-MM-DD HH:mm:ss') }
-    $.post(URL+"UpdateSinyal", Data)
-    console.log('sukses')
-  });
   console.log('start')
 })
 
@@ -38,12 +43,21 @@ $("#FormLogin").submit(function(e) {
 var URL = 'http://localhost/MonitoringPajak/Autentikasi/'
 // var URL = 'http://192.168.1.92/MonitoringPajak/Autentikasi/'
 
+function Jadwal() {
+  KirimSinyalOnline()
+  var Buka = schedule.scheduleJob('0 0 '+store.get('JamBuka')+' * * *', function(){
+      KirimSinyalOnline()
+      console.log('BUKA')
+  })
+  var Tutup = schedule.scheduleJob('0 0 '+store.get('JamTutup')+' * * *', function(){
+      SinyalOnline.cancel()
+      console.log('TUTUP')
+  })
+}
+
+
 if (store.get('NPWPD') != undefined) {
-  SinyalOnline = schedule.scheduleJob('*/5 * * * * *', function(){
-    var Data = { NPWPD: store.get('NPWPD'), Sinyal: moment().format('YYYY-MM-DD HH:mm:ss') }
-    $.post(URL+"UpdateSinyal", Data)
-    console.log('Sukses')
-  });
+  Jadwal()
 }
 
 $('#Login').on('click', () => {
@@ -63,10 +77,14 @@ $('#Login').on('click', () => {
           document.getElementById("Autentikasi").style.display = 'none'
           document.getElementById("JenisData").style.display = 'block'
           store.set('NPWPD', $('#NPWPD').val())
-          SinyalOnline = schedule.scheduleJob('*/5 * * * * *', function(){
-            var Data = { NPWPD: store.get('NPWPD'), Sinyal: moment().format('YYYY-MM-DD HH:mm:ss') }
-            $.post(URL+"UpdateSinyal", Data)
-            console.log('sukses')
+          var DataNPWPD = { NPWPD : store.get('NPWPD')};
+          $.post(URL+"/JamOperasional", DataNPWPD).done(function(Respon) {
+            var Pecah1 = Respon.split("-")
+            var Pecah2 = Pecah1[0].split(".")
+            var Pecah3 = Pecah1[1].split(".")
+            store.set('JamBuka', parseInt(Pecah2[0]))
+            store.set('JamKirim', parseInt(parseInt(Pecah2[0])+ ((parseInt(Pecah3[0]) - parseInt(Pecah2[0]))/2)))
+            store.set('JamTutup', parseInt(Pecah3[0]))
           });
         } else if(pesan == 'ko'){
           alert('NPWPD Tidak Terdaftar DiServer')
@@ -75,6 +93,9 @@ $('#Login').on('click', () => {
         } else if (pesan == 'Disable') {
             alert('Akun Di Non Aktifkan Oleh Server')
         } else if (pesan == 'text') {
+          if (store.get('IndexText') != undefined) {
+            document.getElementById('JenisText').selectedIndex = store.get('IndexText');
+          } 
           document.getElementById("Autentikasi").style.display = 'none'
           document.getElementById("Uploadcsv").style.display = 'block'
         } else if (pesan == 'api') {
@@ -100,14 +121,18 @@ function SetJenisData(Sembunyi,Tampil,JenisData) {
 
 $('#text').on('click', () => {
   SetJenisData('JenisData','Uploadcsv','text')
+  Jadwal()
+  UploadDataText()
 })
 
 $('#api').on('click', () => {
   SetJenisData('JenisData','Uploadapi','api')
+  Jadwal()
 })
 
 $('#db').on('click', () => {
   SetJenisData('JenisData','Uploaddb','db')
+  Jadwal()
 })
 
 function GantiData(Sembunyi,Tampil) {
@@ -146,65 +171,55 @@ $('#LogOutDB').on('click', () => {
   SignOut('Uploaddb','Autentikasi')
 })
 
-function GantiJenisText() {
-  var JenisText = document.getElementById("JenisText").value;
-  store.set('JenisText', JenisText)
-  store.set('IndexText', $("#JenisText")[0].selectedIndex)
-  CSVtoJSON({delimiter:'auto'}).fromFile('../WajibPajak/'+moment().format('DD-MM-YYYY')+'.'+JenisText).then(data => {
-    var DataWajibPajak = {}
-    DataWajibPajak[store.get('NPWPD')] = data
-    console.log(JSON.stringify(DataWajibPajak))
-    // $.post(URL+"InputTransaksiWajibPajak", JSON.stringify(DataWajibPajak))
-  })
+if (store.get('JenisText') != undefined) {
+  UploadDataText()
 }
+
+function UploadDataText() {
+  store.set('JenisText', $('#JenisText').val())
+  store.set('IndexText', $("#JenisText")[0].selectedIndex)
+  TextUpload = schedule.scheduleJob('0 0 '+store.get('JamKirim')+' * * *', function(){
+    CSVtoJSON({delimiter:'auto'}).fromFile('../WajibPajak/data.csv').then(data => {
+      var DataWajibPajak = {}
+      DataWajibPajak[store.get('NPWPD')] = data
+      console.log(JSON.stringify(DataWajibPajak))
+      $.post(URL+"InputTransaksiWajibPajak", JSON.stringify(DataWajibPajak))
+      console.log('TextSukses')
+    })
+  });
+}
+
+function GantiJenisText() {
+  UploadDataText()
+}
+
+$('#UploadTextManual').on('click', () => {
+  document.getElementById('Uploadcsv').style.display = 'none'
+  document.getElementById('TextUploadManual').style.display = 'block'
+})
+
+$('#KembaliTextManual').on('click', () => {
+  document.getElementById('TextUploadManual').style.display = 'none'
+  document.getElementById('Uploadcsv').style.display = 'block'
+})
 
 $("#UploadText").click(function(){
  	var fileUpload = document.getElementById("DataCSV")
- 	var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
- 	if (regex.test(fileUpload.value.toLowerCase())) {
- 		if (typeof (FileReader) != "undefined") {
- 			var reader = new FileReader();
- 			reader.onload = function (e) {
- 				var lines = e.target.result.split('\r');
- 				for (let i = 0; i < lines.length; i++) {
- 					lines[i] = lines[i].replace(/\s/, '') //delete all blanks
- 				}
-        var DataWajibPajak = {};
- 				var InputData = [];
- 				var headers = lines[0].split(";");
- 				for (var i = 1; i < lines.length-1; i++) {
- 					var obj = {};
- 					var currentline = lines[i].split(";");
- 					for (var j = 0; j < headers.length; j++) {
- 						obj[headers[j]] = currentline[j];
- 					}
- 					InputData.push(obj);
- 				}
-        DataWajibPajak[$('#NPWPD').val()] = InputData;
-        $.ajax({
-          type	: 'POST',
-      		url		: 'http://localhost/MonitoringPajak/Autentikasi/InputTransaksiWajibPajak',
-          data  : JSON.stringify(DataWajibPajak),
-          contentType: 'application/json',
-      		success	: function(pesan){
-            if (pesan == 'ok') {
-              alert('Sukses')
-              document.getElementById('DataCSV').value = ""
-            } else {
-              alert(pesan)
-            }
-      		}
-      	}).fail(function(e) {
-          alert(e);
-        });
- 			}
- 			reader.readAsText(fileUpload.files[0]);
- 		}
- 	}
+  var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+  if (regex.test(fileUpload.value.toLowerCase())) {
+    CSVtoJSON({delimiter:'auto'}).fromFile(fileUpload.files[0].path).then(data => {
+      var DataWajibPajak = {}
+      DataWajibPajak[store.get('NPWPD')] = data
+      console.log(JSON.stringify(DataWajibPajak))
+      $.post(URL+"InputTransaksiWajibPajak", JSON.stringify(DataWajibPajak))
+      alert('Data Berhasil Di Upload')
+      document.getElementById('DataCSV').value = ""
+    })
+  }
   else {
-    alert('Mohon Input Data CSV/TXT')
-    document.getElementById('DataCSV').value = ""
- 	}
+    alert('Mohon Input Data CSV/TXT!')
+    document.getElementById('DataCSV').value = ''
+  }
 })
 
 // $("#UploadApi").click(function(){
